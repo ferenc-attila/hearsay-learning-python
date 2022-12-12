5ac5b7a69461a8d125f5e181880874e93d7bbf12
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-12 16:02:49 +0100

    variables

---
+++ hello.py
@@ -0,0 +1,18 @@
+print('Just for practice!')
+
+a = 100
+print(a)
+
+b = 'Attila'
+print('Hello ' + b + '!')
+
+x = 100
+y = 100
+
+print(id(x))
+print(id(y))
+print(id(a))
+
+x = '100'
+
+print(id(x))
