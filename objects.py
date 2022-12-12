0208af4db93993dbe0842f57c3cda262d5517d9e
ferenc-attila <attila.ferenc.80@gmail.com>
2022-12-12 16:21:59 +0100

    objects and type conversion

---
+++ objects.py
@@ -0,0 +1,18 @@
+x = 'hello'
+
+print(id(x))
+print(type(x))
+print(type(50))
+print(type(100.54))
+print(type('100'))
+
+a = '100'
+y = int(a)
+print(type(y))
+print(y)
+
+b = 120
+z = float(b)
+
+print(type(z))
+print(z)
